85f6a3f4e1397ebf89a97e4387e64b38c1255573
tfcmahmoud <69767914+tfcmahmoud@users.noreply.github.com>
2021-06-13 14:36:35 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1042,13 +1042,13 @@ client.on('message', message => {
           let money1 = db.fetch(`money_${message.author.id}`)
           if (money1 === null) money1 = 0;
           console.log(chalk.green(`${message.author.username} has $${money1}`))
-          return message.channel.send(`**:bank: |  ${message.author.username}, your account balance is \`$${money1}\`.**`)
+          return message.channel.send(`:bank: |**  ${message.author.username}, your account balance is \`$${money1}\`.**`)
         }
         if (message.author.id === user.id) {
           let money1 = db.fetch(`money_${message.author.id}`)
           if (money1 === null) money1 = 0;
           console.log(chalk.green(`${message.author.username} has $${money1}`))
-          return message.channel.send(`**:bank: |  ${message.author.username}, your account balance is \`$${money1}\`.**`)
+          return message.channel.send(`:bank: |**  ${message.author.username}, your account balance is \`$${money1}\`.**`)
           }
         let money = db.fetch(`money_${user.id}`) //All Copyrights Goes To TFC Mahmoud And Ottawa Codes 2021-2022
         if (money === null) money = 0;
